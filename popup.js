20b4153c12e9e0ac3c138fe97e1ca78542555b37
lofijay <anirudhyemekar2001@gmail.com>
2022-04-16 01:14:19 +0530

    1.0

---
+++ popup.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector('.colorselector');
+
+    btn.addEventListener('click',() =>{
+        let [tab] chrome,tabs,query ({ active: true, currentWindow: true });
+        console.log(tab)
+});
